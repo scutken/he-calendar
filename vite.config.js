e3f128721424a601ffee2ec80450270e7acf3b79
Kayu Tse <scut_ken@qq.com>
2026-01-26 23:24:42 +0800

    docs: add github actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './'
+  base: './' // 保持相对路径，或者根据仓库名设置为 '/he-calendar/'
 })
